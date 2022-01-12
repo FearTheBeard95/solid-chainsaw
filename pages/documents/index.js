8d2fbc0e01985082b247d4defe495f67c25e1fe3
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-01-12 12:18:40 +0200

    Customize Documents page

--- pages/documents/index.js
+++ pages/documents/index.js
@@ -1,7 +1,9 @@
+import Link from 'next/link';
 import React from 'react';
 import Layout from '../../layouts/main';
+import { getDocuments } from '../../lib/documents';
 
-export default function documents() {
+export default function documents({ documents }) {
   return (
     <Layout title={'Documents'} authenticated={true}>
       <section className='pb-20 bg-blueGray-200 -mt-24'>
@@ -10,15 +12,38 @@ export default function documents() {
         <div className='container mx-auto px-4'>
           <div className='flex flex-wrap'></div>
 
-          <div className='flex flex-wrap items-center mt-32'>
+          <div className='flex flex-wrap items-start justify-center mt-32'>
             <div className='w-full md:w-5/12 px-4 mr-auto ml-auto'>
-              <h1 className='text-3xl mb-2 font-semibold leading-normal'>
-                Under development
-              </h1>
+              <ul>
+                {documents.map((doc) => (
+                  <li key={doc.documents.name}>
+                    <h4 className='text-3xl font-normal leading-normal mt-0 mb-2 text-blueGray-800'>
+                      {doc.documents.name}
+                      {'  '}
+                      <Link href={doc.documents.link} key={doc.documents.name}>
+                        <a target={'_blank'}>
+                          <i className='fas fa-download text-blueGray-800'></i>
+                        </a>
+                      </Link>
+                    </h4>
+                    <hr className='my-6 border-blueGray-300' />
+                  </li>
+                ))}
+              </ul>
             </div>
           </div>
         </div>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allDocuments = await getDocuments();
+  console.log(allDocuments);
+  return {
+    props: {
+      documents: allDocuments,
+    },
+  };
 }
