4bebc713c3ea4973483317c25b25bdb491788d0f
Carlos Sibalatani <carlos.sibalatani@mhss.gov.na>
2022-01-24 20:09:03 +0200

    Add sendgrid intergration

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,23 @@
+const mail = require('@sendgrid/mail');
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+mail.setApiKey(process.env.SENDGRID_API_KEY);
+
 export default function handler(req, res) {
-  res.status(200).json({ status: 'OK' });
+  const body = JSON.parse(req.body);
+  const message = `
+  Name: ${body.name}\r\n
+  Email: ${body.email}\r\n
+  Message: ${body.message}
+`;
+
+  mail
+    .send({
+      to: 'sibalatanics@outlook.com',
+      from: 'from.name@shackdwellers.com',
+      subject: 'New Message from shackdwellers site!',
+      text: message,
+      html: message.replace(/\r\n/g, '<br>'),
+    })
+    .then(() => res.status(200).json({ status: 'OK' }));
 }
